39e8dba4beaa422cd305fccd3c50f587d9dec9c6
liuyi <liuxy0551@qq.com>
2020-01-03 17:08:17 +0800

    add: GithubPage docs

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,8 @@ module.exports = {
   devServer: {
     disableHostCheck: true,//解决127.0.0.1指向其他域名时出现"Invalid Host header"问题
   },
+  publicPath: process.env.NODE_ENV === 'production' ? '/my-vue/' : '/',
+  outputDir: 'docs',
   chainWebpack: config => {
     if (process.env.use_analyzer) {
       config
