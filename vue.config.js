38c86989c9f4fe1e03c15f6ce51a32fec07c55b9
liuyi <liuxy0551@qq.com>
2020-07-14 20:08:47 +0800

    部署服务器

--- vue.config.js
+++ vue.config.js
@@ -7,8 +7,6 @@ module.exports = {
   devServer: {
     disableHostCheck: true,//解决127.0.0.1指向其他域名时出现"Invalid Host header"问题
   },
-  publicPath: process.env.NODE_ENV === 'production' ? '/my-vue/' : '/',
-  outputDir: 'docs',
   chainWebpack: config => {
     config.resolve.alias.set('vue$', 'vue/dist/vue.common.js') // key,value自行定义，比如.set('@@', resolve('src/components'))
       .set('@', resolve('src'))
